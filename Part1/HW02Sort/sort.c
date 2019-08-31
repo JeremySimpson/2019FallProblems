7cba92cd42bf88869266273525efb3397c84e149
Jeremy M Simpson <simpso89@ecegrid-thin5.ecn.purdue.edu>
2019-08-31 14:55:24 -0400

    final version

--- Part1/HW02Sort/sort.c
+++ Part1/HW02Sort/sort.c
@@ -28,38 +28,30 @@ bool checkOrder(int * arr, int size)
 #ifdef TEST_SORT
 void ssort(int * arr, int size)
 {
-    int pos1;
-    int pos2;
-    int swapPos;
-    int tempPos;
+    int pos1; //lower position
+    int pos2; //higher position
+    int swapPos; //temporary position used to reorder terms
+    int tempPos; //temporary position used to store the lower position
+    int lastTerms; //index of last term in array
     
-    
-    printf("The Sorted Array is:\n");
-    
-    for (pos1 = 0; pos1 < (size); pos1++)
+    lastTerms = size - 1; //converts size of array to last index
+
+    for (pos1 = 0; pos1 < lastTerms; pos1++) //continues for all indexes except for last
     {
-        tempPos = pos1;
+        tempPos = pos1; //saves lower position as the potential lower value
         
-        for (pos2 = pos1 + 1; pos2 <= size ; pos2++)  
+        for (pos2 = pos1 + 1; pos2 <= lastTerms ; pos2++) //repeats for all indees except for first 
         {
             if (arr[tempPos] > arr[pos2])
-                tempPos = pos2;
+                tempPos = pos2; //saves higher position if value at index is lower
         }
-        if (tempPos != pos1)
+        if (tempPos != pos1) //performs if value in higher position is lower
         {
             swapPos = arr[pos1];
             arr[pos1] = arr[tempPos];
-            arr[tempPos] = swapPos;
+            arr[tempPos] = swapPos; //these three lines swap the two values
         }
     }
-    
-    pos1 = 0;
-    while (pos1 != size)
-    {
-        printf("%d\n",arr[pos1]);
-        pos1 ++;
-    }
-    
     return;
 }
 #endif
