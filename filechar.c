6e43b99dd0bbe81ae7a3e74a894ae25f8819f2cb
JeremySimpson <54729389+JeremySimpson@users.noreply.github.com>
2019-09-09 00:09:03 -0400

    Add files via upload

---
+++ filechar.c
@@ -0,0 +1,86 @@
+// ***
+// *** You MUST modify this file, only the ssort function
+// ***
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#ifdef TEST_COUNTCHAR
+bool countChar(char * filename, int * counts, int size)
+{
+  // open a file whose name is filename for reading
+  
+  // if fopen fails, return false. Do NOT fclose
+  
+  if (argc < 2)
+  {
+	  return false;
+  }
+  
+  FILE * fptr = fopen(filename, "r");
+  
+  if (fptr == NULL)
+  {
+	  return false;
+  }
+  
+  // if fopen succeeds, read every character from the file
+  int onechar;
+  /*
+  do
+  {
+	  onechar = fgetc(fptr);
+	  //printf("%c %d\n", onechar, onechar);
+  } while (onechar != EOF);
+  */
+  //int charcount [256] = {0};
+  while (! feof(fptr));
+  {
+	onechar = fgetc(fptr);
+	if (onechar != EOF)
+	{
+		counts[onechar] ++;
+    }
+    printf("%c %d\n", onechar, onechar);
+  }  
+  int ind;
+  for (ind = 0; ind < size; ind++);
+  {
+	  if (charcount[ind] !=0)
+	  {
+		  printf("%c occurs %d times\n", ind, charcount[ind]);
+	  }
+  }
+  fclose (fptr);
+  
+  
+  //
+  // if a character (call it onechar) is between
+  // 0 and size - 1 (inclusive), increase
+  // counts[onechar] by one
+  // You should *NOT* assume that size is 256
+  // reemember to call fclose
+  // you may assume that counts already initialized to zero
+  // size is the size of counts
+  // you may assume that counts has enough memory space
+  //
+  // hint: use fgetc
+  // Please read the document of fgetc carefully, in particular
+  // when reaching the end of the file
+  //
+  return true;
+}
+#endif
+
+#ifdef TEST_PRINTCOUNTS
+void printCounts(int * counts, int size)
+{
+  // print the values in counts in the following format
+  // each line has three items:
+  // ind, onechar, counts[ind]
+  // ind is between 0 and size - 1 (inclusive)
+  // onechar is printed if ind is between 'a' and 'z' or
+  // 'A' and 'Z'. Otherwise, print space
+  // if counts[ind] is zero, do not print
+}
+#endif
