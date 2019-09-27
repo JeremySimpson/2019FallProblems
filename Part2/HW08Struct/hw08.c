eb1bc154736e070996b9350232ec5cf71efa5a85
JeremySimpson <54729389+JeremySimpson@users.noreply.github.com>
2019-09-27 14:01:46 -0400

    Update hw08.c

--- Part2/HW08Struct/hw08.c
+++ Part2/HW08Struct/hw08.c
@@ -15,9 +15,23 @@ int countVector(char * filename)
   // The input is a binary file. You must use fread.
   // You must not use fscanf(, "%d", ) 
   //
+  FILE * fptr = fopen(filename,"r");
+  if (fptr == NULL)
+	  return -1;
+  
   // If fopen fails, return -1
   //
   //
+  int sum = 0;
+  Vector temp;
+  int count = 0;
+  
+  while (fread(&temp, sizeof(Vector), 1, fptr) != 0)
+  {
+	sum++;
+  }
+  fclose(fptr);
+  return sum;
   // For the mode of fopen, you may use "r" without b
   //
 }
@@ -26,15 +40,28 @@ int countVector(char * filename)
 #ifdef TEST_READVECTOR
 bool readVector(char* filename, Vector * vecArr, int size)
 {
+  FILE * fptr = fopen(filename, "r")
   // if fopen fails, return false
+  if (fptr == NULL)
+	  return false;
   // read Vectors from the file.
   // 
+  int numInt;
+  numInt = fread(vecArr, sizeof(Vector), size, fptr);
   //
   // if the number of integers is different from size (too
   // few or too many) return false
   // 
+  //int numInt = 0;
+  if (numInt != size)
+  {
+	  fclose(fptr);
+	  return false;
+  }
+  
   // if everything is fine, fclose and return true
-
+  fclose(fptr);
+  return true;
 }
 #endif
 
@@ -44,6 +71,13 @@ int compareVector(const void *p1, const void *p2)
   // compare the x attribute first
   // If the first vector's x is less than the second vector's x
   // return -1
+  if (*(int*)p1 < (*(int*)p2)
+	  return -1;
+  if (*(int*)p1 > *(int*)p2)
+	  return 1;
+  if (*(int*)p1 == *(int*)p2)
+  {
+  }
   // If the first vector's x is greater than the second vector's x
   // return 1
   // If the two vectors' x is the same, compare the y attribute
@@ -65,17 +99,31 @@ int compareVector(const void *p1, const void *p2)
 #ifdef TEST_WRITEVECTOR
 bool writeVector(char* filename, Vector * vecArr, int size)
 {
+  FILE * fptr = fopen(filename, "r")
   // if fopen fails, return false
+  if (fptr == NULL)
+	  return false;
+  int numWrite = 0;
+  //numWrite = fwrite(vecArr, size, sizeof(Vector), fptr);
+  numWrite = fwrite(vecArr, sizeof(Vector), size, fptr);
+  
   // write the array to file using fwrite
   // need to check how many have been written
   // if not all are written, fclose and return false
   // 
+  if (numWrite != size)
+  {
+	  fclose(fptr);
+	  return false;
+  }
+  fclose(fptr);
+  return true;
   // fclose and return true
 }
 #endif
 
 // This function is provided to you. No need to change
-void printVector(Vector * vecArr, int size)
+void printVector(Vector * vecArr, int size) 
 {
   int ind = 0;
   for (ind = 0; ind < size; ind ++)
