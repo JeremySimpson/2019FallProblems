83bc04955a55d657d13dfdb15124f811cb2fee44
JeremySimpson <54729389+JeremySimpson@users.noreply.github.com>
2019-10-02 17:45:15 -0400

    Update hw09.c

--- Part2/HW09MergeSort/hw09.c
+++ Part2/HW09MergeSort/hw09.c
@@ -31,64 +31,75 @@ bool readData(char * filename, int * * arr, int * size)
 {
   // use fopen to open the file for read
   // return false if fopen fails
-
-
-
+  FILE * fptr = fopen(filename, "r");
+  
+  if (fptr == NULL)
+	  return false;
 
   // use fseek to go to the end of the file
   // check whether fseek fails
   // if fseek fails, fclose and return false
 
-
-
-
-
-  // use ftell to determine the size of the file
-
-
-
+  seekStat = fseek(fptr, 0, SEEK_END);
   
+  if (seekStat != 0)
+  {
+	  fclose(fptr);
+	  return false;
+  }
+  
+  // use ftell to determine the size of the file
+  size = ftell(fptr);
+
   // use fseek to go back to the beginning of the file
   // check whether fseek fails
-
-
-
   
+  seekStat = fseek(fptr, size, SEEK_SET);
   // if fseek fails, fclose and return false
-
-
-
+  
+  if (seekStat != 0)
+  {
+	  fclose(fptr);
+	  return false;
+  }
+  
   // the number of integers is the file's size divided by
   // size of int  
-
-
-
-
   
-
+  numInt = size / sizeof(int);
+ 
   // allocate memory for the array
-
+  
+  * arr = malloc(sizeof(int) * numInt); 
 
   // if malloc fails, fclose and return false
 
+  if (arr == NULL)
+  {
+	  fclose(fptr);
+	  return false;
+  }
 
 
   // use fread to read the number of integers in the file
 
-
-
-
+  readStat = fread(arr, sizeof(int), numInt, fptr);
+  
   // if fread does not read the correct number
   // release allocated memory
   // fclose
   // return false
-
-
-
-
   
+  if (readStat != numInt)
+  {
+	  free(arr);
+	  fclose(fptr);
+	  return false;
+  }
+
   // if fread succeeds
   // close the file
+  fclose(fptr);
 
   
   // update the argument for the array address
@@ -96,7 +107,7 @@ bool readData(char * filename, int * * arr, int * size)
 
   
   // update the size of the array
-
+  size = numInt;
 
   
   return true;
@@ -110,32 +121,28 @@ bool readData(char * filename, int * * arr, int * size)
 bool writeData(char * filename, const int * arr, int size)
 {
   // fopen for write
-
+  FILE * fptr = fopen(filename, "w");
 
 
   // if fopen fails, return false
-
-
+  if (fptr == NULL)
+	  return false;
 
   // use fwrite to write the entire array to a file
-
-
-
-  // check whether all elements of the array have been written
-
-
-
-  // fclose
-
-
+  numWrite = fwrite(arr, sizeof(int), size, fptr);
   
+  // check whether all elements of the array have been written
+  // fclose
   // if not all elements have been written, return false
-
-
-
+  if (numWrite != size)
+  {
+	  fclose(fptr);
+	  return false;
+  }
+ 
   // if all elements have been written, return true
 
-
+  return true;
 
 
 
@@ -167,7 +174,17 @@ static void merge(int * arr, int l, int m, int r)
 #endif
 
   // if one or both of the arrays are empty, do nothing
-
+  int i;
+  int j;
+  int k;
+  if ((l == m) && (m == r))
+	  return;
+  
+  int num1;
+  int num2;
+  
+  int num1 = m - l + 1;
+  int num2 = r - m;
 
 
 
@@ -175,7 +192,30 @@ static void merge(int * arr, int l, int m, int r)
   // Hint: you may consider to allocate memory here.
   // Allocating additiional memory makes this function easier to write
 
+  int L[num1], R[num2];
+  
+  for (i = 0; i < num1; i++)
+    L[i] = arr[l+i];
+  for (j = 0; j < num2; j++)
+	  R[j] = arr[m+1+j];
 
+  i = 0;
+  j = 0;
+  k = l;
+  
+  while (i < num1)
+  {
+	  arr[k] = L[i];
+	  i++;
+	  k++;
+  }
+  
+  while (j < num2)
+  {
+    arr[k] = R[j];
+	j++;
+	k++;
+  }
 
 
   // merge the two parts (each part is already sorted) of the array
@@ -217,12 +257,23 @@ void mergeSort(int arr[], int l, int r)
 #endif
 
   // if the array has no or one element, do nothing
-
-
-
+ if ((l == r) || (l == (r-1)))
+	 return;
+ 
   // divide the array into two arrays
+  if (l < r)
+  {
+	  int m = 1 + (r-1)/2;
+	  
   // call mergeSort with each array
+  mergeSort(arr, l, m);
+  mergeSort(arr, m+1, r);
+  
   // merge the two arrays into one
+  merge(arr, l, m, r);
+  }
+  
+  
   
 
 
